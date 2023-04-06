effe9a1444addae2c834de7c933998edc9f626a5
ThanhTC21 <thanhtc21@fpt.com.vn>
2023-04-06 11:08:15 +0700

    add envern chanel

--- ios/Classes/MobimapPlugin.swift
+++ ios/Classes/MobimapPlugin.swift
@@ -6,6 +6,10 @@ public class MobimapPlugin: NSObject, FlutterPlugin {
       let channel = FlutterMethodChannel(name: ChanelName.method.rawValue, binaryMessenger: registrar.messenger())
     let instance = MobimapPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    let eventGPS = FlutterEventChannel(name: ChanelName.eventGPS.rawValue, binaryMessenger: registrar.messenger())
+    eventGPS.setStreamHandler(instance)
+    let eventNetwork = FlutterEventChannel(name: ChanelName.eventNetwork.rawValue, binaryMessenger: registrar.messenger())
+    eventNetwork.setStreamHandler(instance)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
