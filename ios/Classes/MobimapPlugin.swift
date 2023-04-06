0086041d9274ac48c43873e568ceb5bce9a1403c
ThanhTC21 <thanhtc21@fpt.com.vn>
2023-04-06 14:30:02 +0700

    update plugin

--- ios/Classes/MobimapPlugin.swift
+++ ios/Classes/MobimapPlugin.swift
@@ -1,18 +1,28 @@
 import Flutter
 import UIKit
 
-public class MobimapPlugin: NSObject, FlutterPlugin {
+public class MobimapPlugin: NSObject, FlutterPlugin,FlutterStreamHandler {
+    var sink: FlutterEventSink?
   public static func register(with registrar: FlutterPluginRegistrar) {
-      let channel = FlutterMethodChannel(name: ChanelName.method.rawValue, binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: ChanelName.method.rawValue, binaryMessenger: registrar.messenger())
     let instance = MobimapPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
-    let eventGPS = FlutterEventChannel(name: ChanelName.eventGPS.rawValue, binaryMessenger: registrar.messenger())
-    eventGPS.setStreamHandler(instance)
-    let eventNetwork = FlutterEventChannel(name: ChanelName.eventNetwork.rawValue, binaryMessenger: registrar.messenger())
-    eventNetwork.setStreamHandler(instance)
+    let eventGPSChannel = FlutterEventChannel(name: ChanelName.eventGPS.rawValue, binaryMessenger: registrar.messenger())
+    eventGPSChannel.setStreamHandler(instance)
+    let eventNetworkChannel = FlutterEventChannel(name: ChanelName.eventNetwork.rawValue, binaryMessenger: registrar.messenger())
+    eventNetworkChannel.setStreamHandler(instance)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
+  public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+      self.sink = events
+      return nil
+   }
+
+   public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+      self.sink = nil
+      return nil
+   }
 }
